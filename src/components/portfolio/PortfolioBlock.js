54bbe8c9d017ce372c559ff914defba5b88bf7b0
Avi Walia <av9.walia@gmail.com>
2024-01-06 15:16:16 -0500

    Updated portfolio

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -5,14 +5,14 @@ import {Box} from "@mui/material";
 function PortfolioBlock(props) {
    const {image, description, source, title} = props;
    return (
-      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-         <Box component={'img'} src={image} alt={'mockup'}/>
-         <h1 style={{fontSize: '2rem'}}>{title}</h1>
-         <p style={{fontSize: '1rem'}}>{description}</p>
+      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} margin={'10px'}>
+         <Box component={'img'} src={image} alt={'mockup'} height={545}/>
+         <h1 style={{fontSize: '2.5rem', marginTop: '10px'}}>{title}</h1>
+         <p style={{fontSize: '1.2rem', margin: '15px', textAlign: 'justify', marginBottom: '0px'}}>{description}</p>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
+               <IconLink link={source} title={'Github'} icon={'fa fa-code'}/>
             </Box>
          </Box>
       </Box>
--- src/info/Info.js
+++ src/info/Info.js
@@ -1,7 +1,7 @@
 import mazeRunner from "../img/mazeRunner.png"
 import uFound from "../img/uFound.png"
-// import mock3 from "../img/mock3.png"
-// import mock4 from "../img/mock4.png"
+import spotifyManager from "../img/spotify.png"
+import cineMatch from "../img/cinematch.png"
 
 // export let colors = ["rgb(255, 140, 66)", "rgb(88, 124, 153)"];
 export let colors = ["rgb(255, 140, 66)", "rgb(60, 92, 119)"];
@@ -53,8 +53,8 @@ export const info = {
     bio: "Hello! My name is Avi Walia. I am a second-year computer science student at UoFT completing a specialist degree in data science. I most recently worked as a full-stack developer in a summer coop position at Third Eye Solutions. I love playing and watching basketball (go Raptors!!) and going on walks with my dog. ",
     skills:
         {
-            proficientWith: ['Python', 'Java', 'Flask', 'SQL', 'Github', 'NumPy', 'Javascript', 'React', 'Html', 'Css'],
-            exposedTo: ['GCP Firebase', 'Springboot', 'Maven', 'scikit-learn']
+            proficientWith: ['Python', 'Java', 'SQL', 'Pandas', 'HTML/CSS', 'Javascript', 'React', 'NumPy', 'Flask', 'Git'],
+            exposedTo: ['scikit-learn', 'GCP Firebase', 'Spring Boot', 'Apache Maven']
         }
     ,
     hobbies: [
@@ -81,16 +81,28 @@ export const info = {
     ],
     portfolio: [ 
         {
-            title: "Maze Runner",
-            description: "",
-            source: "https://github.com/Avi04w/Maze",
-            image: mazeRunner,
+            title: "Spotify Manager",
+            description: "I created a spotify spoof that allows users to control their Spotify playback on any device. The app will detect any device that you are logged in to and your current playback will be displayed on the player. It makes requests to the Spotify API such as pause, play, skip, etc., using OkHttp and uses the OAuth 2.0 Authentication Code Flow to authenticate users and access their Spotify accounts. I created this app using Java and the GUI was made with Java Swing. Something else about me is that this is my favourite song!",
+            source: "https://github.com/Avi04w/SpotifyManager",
+            image: spotifyManager,
+        },
+        {
+            title: "CineMatch",
+            description: "This web app takes a person's movie watch history (can be downloaded from streaming services such as Netflix) and recommends similar movies to watch. I built the movies recommendation algorithm using machine learning tools in Python (NumPy, pandas, and scikit-learn) and the front end was created using ReactJS and HTML/CSS. The movie recommendation data was sent from Python to the front end using Flask as I created my own API to accomplish this. Each movie is assigned a sequence of numbers based on their title, genre, and description, and are treated as vectors. The similarities between each movie are computed using the cosine similarity function and assigned a value to be compared altogether.",
+            source: "https://github.com/Avi04w/Cinematch",
+            image: cineMatch,
         },
         {
             title: "UFound",
-            description: "",
+            description: "This is a React JavaScript web application. It is an online lost and found for Uoft Students. If users find a lost item on campus, they can create a posting with an image of the item along with the location of where they found it. The information is stored using a NoSQL document database, Google FireStore.",
             source: "https://github.com/Avi04w/uoft-lost-and-found",
             image: uFound
+        },
+        {
+            title: "Maze Runner",
+            description: "This is a Python game I created that creates mazes for you to solve. It does this using graphs and nodes and uses a recursion and DFS algorithm to find the solution if you cannot find it. The front end is programmed using the PyGame library.",
+            source: "https://github.com/Avi04w/Maze",
+            image: mazeRunner,
         },
     ]
 }
